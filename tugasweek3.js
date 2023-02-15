f32130c791db2f1430d44169b370d8f85d45a248
yaserafrizzal <yaserafrizzal@gmail.com>
2023-02-15 22:50:08 +0700

    tugasweek3

--- tugasweek3.js
+++ tugasweek3.js
@@ -1,48 +1,64 @@
 const cekharikerja = (day) => {
-    return new promise ((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-            const dataday = ['senin', 'selasa', 'rabu', 'kamis', 'jumat']
-            return item === day
-        })
+            const dataDay = ['senin', 'selasa', 'rabu', 'kamis', 'jumat']
+            let cek = dataDay.find((item) => {
+                return item === day
+            })
             if (cek){
                 resolve(cek)
             }else {
-                reject(new error ('hari ini bukan hari kerja'))
+                reject(new Error ('hari ini bukan hari kerja'))
             }
-    },3000)
+        },3000)
+    })
 }
 
+const hasil = cekharikerja('minggu')
+    .then(result => console.log(result))
+    .catch(error => console.log(error.message))
+    
+async function harikerja(x) {
+    try {
+      const result = await cekharikerja(x);
+      console.log(result);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  
+  harikerja('senin');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const getmonth = (callback) => {
+const getMonth = (callback) => {
     setTimeout(() => {
         let error = false
-        let month = [ 'januari', 'februari', 'maret', 'april', 
-                        'mei', 'juni', ' juli', 'agustus', 'september',
-                    'oktober', 'november', 'desember']
-        if (!error) {
-            callback (null,month)
-        }else {
-            callback(new error('sorry data not found'), [])
+        let month = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desemeber']
+        if (!error){
+            callback(null,month)
+        } else {
+            callback(new Error ('Sorry Data Not Found'), [])
         }
-    }, 40000)
-
+    }, 4000)
 }
+
+function myFunction(month) {
+    return console.log(month)
+}
+
+const showMonth = (x,y) => {
+    if (!x){
+        y.map(myFunction)
+    }else {
+        console.log(x.message)
+    }
+}
+
+getMonth(showMonth)
+
+async function getData(){
+    res = await fetch('https://jsonplaceholder.typicode.com/users');
+    data = await res.json();
+    console.log(data.map(index => {return index.name}));
+}
+
+getData();
