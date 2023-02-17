ac31b4045379ded1bce556a42963c574bf308a97
yaserafrizzal <yaserafrizzal@gmail.com>
2023-02-17 10:00:44 +0700

    penambahan

--- tugasweek3.js
+++ tugasweek3.js
@@ -1,3 +1,4 @@
+//nomer 1
 const cekharikerja = (day) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -28,7 +29,10 @@ async function harikerja(x) {
 }
   
 harikerja('senin');
+// thencatch digunakan untuk promise (resolve, reject)
+// trycatch digunakan untuk kode error dan untuk tetap dijalankan
 
+//nomer 2
 const getMonth = (callback) => {
     setTimeout(() => {
         let error = false
@@ -38,7 +42,7 @@ const getMonth = (callback) => {
         } else {
             callback(new Error ('Sorry Data Not Found'), [])
         }
-    }, 4000)
+    }, 3000)
 }
 
 function myFunction(month) {
@@ -55,6 +59,70 @@ const showMonth = (x,y) => {
 
 getMonth(showMonth)
 
+//nomer3
+const motormatic = (motor) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const dataMotor = ['beat', 'mio', 'vespa', 'nmax', 'vario']
+            let cek = dataMotor.find((item) => {
+                return item === motor
+            })
+            if (cek){
+                resolve(cek)
+            }else {
+                reject(new Error ('bukan motor matic'))
+            }
+        },3000)
+    })
+}
+
+const hasil1 = motormatic('supra')
+    .then(result => console.log(result))
+    .catch(error => console.log(error.message))
+    
+async function matic1(x) {
+    try {
+      const result = await motormatic(x);
+      console.log(result);
+    } catch (error) {
+      console.log(error.message);
+    }
+}
+  
+matic1('beat');
+
+const golonganmamalia = (mamalia) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const dataMamalia = ['sapi', 'kelinci', 'rusa', 'kambing', 'kerbau']
+            let cek = dataMamalia.find((item) => {
+                return item === mamalia
+            })
+            if (cek){
+                resolve(cek)
+            }else {
+                reject(new Error ('bukan hewan mamalia'))
+            }
+        },3000)
+    })
+}
+
+const muncul = golonganmamalia('elang')
+    .then(result => console.log(result))
+    .catch(error => console.log(error.message))
+    
+async function termasukmamalia(x) {
+    try {
+      const result = await golonganmamalia(x);
+      console.log(result);
+    } catch (error) {
+      console.log(error.message);
+    }
+}
+  
+termasukmamalia('sapi');
+
+//nomer 4
 async function getData(){
     res = await fetch('https://jsonplaceholder.typicode.com/users');
     data = await res.json();
